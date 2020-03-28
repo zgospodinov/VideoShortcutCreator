08df88f0f71c6deeda6703de44942e6878476a35
zdravki4 <zdravko.gospodinov@gmail.com>
2020-03-28 22:23:02 +0200

    Shortcut names extraction

---
+++ VideoShortcutCreator.Test/ShortcutsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.IO;
+using Test.Logic;
+
+namespace VideoShortcutCreator.Test
+{
+    [TestFixture]
+    public class ShortcutsTests
+    {
+        private FileInfo movie = new FileInfo("The.Mandalorian.S01E05.iNTERNAL.720p.WEB.H264-AMRAP.mp4");
+        private FileInfo movie2 = new FileInfo("The.Mandalorian.S0.mp4");
+
+        [Test]
+        public void GetSeriesSeasonNumber_WithValidName_ReturnsProperValie()
+        {
+            var seriesNumber = movie.GetSeriesSeasonNumber(1);
+
+            Assert.That(seriesNumber, Is.EqualTo("S01E05"));
+        }
+
+        [Test]
+        public void GetSeriesSeasonNumber_WithInValidName_ReturnsIndexValie()
+        {
+            var seriesNumber = movie2.GetSeriesSeasonNumber(1);
+
+            Assert.That(seriesNumber, Is.EqualTo("01"));
+        }
+    }
+}
